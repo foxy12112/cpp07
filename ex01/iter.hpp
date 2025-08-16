90189cdd565e61e9af95aad29ca945915834ed56
foxy12112 <leon.dick1@gmx.de>
2025-08-16 23:17:44 +0200

    added some testst

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,22 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
+/*   By: foxy <foxy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 17:35:43 by ldick             #+#    #+#             */
-/*   Updated: 2025/08/15 17:44:58 by ldick            ###   ########.fr       */
+/*   Updated: 2025/08/16 23:10:01 by foxy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template <typename T>
+#include <iostream>
 
-void iter(T *array, T size, T (*f)(T))
+template <typename T, typename func>
+
+void iter(T *array, int size, func f)
 {
 	for (int i = 0; i < size; i++)
-		f(array[i])
+		f(array[i]);
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,17 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
+/*   By: foxy <foxy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 17:35:06 by ldick             #+#    #+#             */
-/*   Updated: 2025/08/15 17:47:36 by ldick            ###   ########.fr       */
+/*   Updated: 2025/08/16 23:10:46 by foxy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+void speak_i(int j)
+{
+	std::cout << j << std::endl;
+}
+
+void speak_c(char j)
+{
+	std::cout << j << std::endl;
+}
+
 int main (void)
 {
 	int int_array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	::iter(int_array, 10, )
+	char char_array[5] = {'a', 'b', 'c', 'd', 'h'};
+	std::cout << "using int\n" << std::endl;
+	::iter(int_array, 10, speak_i);
+	std::cout << "\nusing char\n" << std::endl;
+	::iter(char_array, 5, speak_c);
 }
